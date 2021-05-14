eb341a1262f8af4fd93496955a20bd9d98e043ce
rhuirhui <68117729+rhuirhui@users.noreply.github.com>
2021-05-14 10:02:24 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     guess = 0
-    basic.showString("My guess is")
     radio.sendNumber(guess)
 })
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
 })
 let guess = 0
 radio.setGroup(1)
-basic.forever(function () {
-	
-})
